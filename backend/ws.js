161685085022c35587944d46d9f7ccf537c5d066
whitemu <whitemu92@163.com>
2020-11-29 22:16:38 +0800

    增加资源文件

--- backend/ws.js
+++ backend/ws.js
@@ -1,12 +1,48 @@
-var ws = require("nodejs-websocket");
-var roomConnetctonList = []
-var allUser = []
+const ws = require("nodejs-websocket");
+const fs = require("fs")
+var allUser = [{
+    userName: '周瑜',
+    userIcon: './assets/0.jpg'
+},{
+    userName: '小乔',
+    userIcon: './assets/1.jpg'
+},{
+    userName: '大乔',
+    userIcon: ''
+},{
+    userName: '武大郎',
+    userIcon: ''
+},{
+    userName: '西门庆',
+    userIcon: ''
+},{
+    userName: '武松',
+    userIcon: ''
+},{
+    userName: '嫦娥',
+    userIcon: ''
+},{
+    userName: '猪八戒',
+    userIcon: ''
+},{
+    userName: '扫地僧',
+    userIcon: ''
+},{
+    userName: '唐僧',
+    userIcon: ''
+},{
+    userName: '孙悟空',
+    userIcon: ''
+}]
+
+var nearMsgList = []
 
 var userIndex = 0
 
+readUserIndexFile()
+
 class ConnHandler {
     userId = 0
-    
     constructor(server, conn) {
         this.server = server;
         this.conn = conn;
@@ -15,37 +51,69 @@ class ConnHandler {
     onEnter(msg) {
         let userId = msg.userId
         if(userId === null) {
-            index ++;
-            let name = "user"+ index
-            this.userId = index
-            this.name = name
+            userIndex ++;
+            this.userId = userIndex
+            this.userName = allUser[userIndex - 1].userName
+            //储存id
+            writeIndex(this.userId.toString())
             // 发消息给conn, 
             let param = {
                 type: "register",
                 data:{
-                    userId: this.index,
-                    userIcon: '',
-                    userName: this.name,
+                    userId: this.userId,
+                    userIcon: allUser[userIndex - 1].userIcon,
+                    userName: this.userName,
                 }
             }
             this.sendMessage(param)
         } else {
             this.userId = userId
-            this.name = "user"+ index
+            this.userName = allUser[this.userId - 1].userName
+        }
+        //下发近10条消息
+        let nearParam = {
+            type: 'msgList',
+            data: nearMsgList
+        }
+        if(nearMsgList.length>0) {
+            this.sendMessage(nearParam)
         }
         // 发广播
         let param = {
             type: 'notify',
             data: {
-                content: this.name + '加入了群聊'
+                content: this.userName + '加入了群聊'
             }
         }
-        this.sendGroupMessage()
+        this.sendGroupMessage(param)
+    }
+
+    onClose() {
+        // 发广播
+        let param = {
+            type: 'notify',
+            data: {
+                content: this.userName + '离开了群聊'
+            }
+        }
+        this.sendGroupMessage(param)
     }
 
     onMsg(msg) {
-        
-        this.sendGroupMessage()
+        let senderId = msg.senderId
+        let param = {
+            type: 'msg',
+            data: {
+                senderId,
+                senderName: allUser[senderId - 1].userName,
+                senderIcon: allUser[senderId - 1].userIcon,
+                content: msg.content,
+                time: msg.time
+            }
+        }
+        //存储近10条消息
+        cacheNearMsg(param)
+        this.sendGroupMessage(param)
     }
 
     sendMessage(msg) {
@@ -59,10 +127,40 @@ class ConnHandler {
     }
 }
 
+function cacheNearMsg(msg) {
+    if(nearMsgList.length === 10) {
+        nearMsgList.shift()
+    }
+    nearMsgList.push(msg)
+}
 
+function writeIndex(index) {
+    fs.writeFile('userIndex.txt', index, 'utf8', function(error){
+        if(error){
+            console.log(error);
+            return false;
+        }
+        console.log('写入成功');
+    })
+    
+}
+
+function readUserIndexFile() {
+    fs.readFile('userIndex.txt',function(error,data){
+        if(error){
+            console.log(error);
+            return false;
+        }
+        //console.log(data);  //data是读取的十六进制的数据。  也可以在参数中加入编码格式"utf8"来解决十六进制的问题;
+        console.log(data.toString());  //读取出所有行的信息 
+        if(data.toString()) {
+            userIndex = data.toString()
+        }
+        console.log(userIndex)
+    })
+}
 
 var server = ws.createServer(function(conn){
-    roomConnetctonList.push(conn)
     var handler = new ConnHandler(server, conn)
     conn.on("text", function (response) {
         const message = JSON.parse(response)
@@ -74,22 +172,11 @@ var server = ws.createServer(function(conn){
         }
     })
     conn.on("close", function (code, reason) {
-        let index = -1;
-        for(let i = 0; i<roomConnetctonList.length; i++) {
-            if(conn == roomConnetctonList[i]) {
-                index = i;
-                break;
-            }
-        }
-        if(index != -1) {
-            roomConnetctonList.splice(index, 1)
-        }
+        handler.onClose()
         console.log("关闭连接")
     });
     conn.on("error", function (code, reason) {
+        handler.onClose()
         console.log("异常关闭")
     });
 }).listen(8001)
-
-
-console.log("WebSocket建立完毕")
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,21 +1,31 @@
 <template>
   <div id="app">
-    <div id="mess">{{statusTips}}</div>
+    <div id="mess">{{ statusTips }}</div>
     <div class="chat-wrapper">
       <!-- message list -->
-      
       <div class="list-wrap">
         <div class="list" v-for="(item, index) in messageList" :key="index">
-          <div class="send">
-            {{item}}
-            <div class="arrow"></div>
+          <div class="list-content" v-if="item.senderId != userId">
+            <img class="icon" :src="item.senderIcon" />
+            <div class="send left">
+              {{ item.content }}
+              <div class="arrow"></div>
+            </div>
+          </div>
+          <div class="list-content" v-else>
+            <div class="send right">
+              {{ item.content }}
+              <div class="arrow"></div>
+            </div>
+            <img class="icon" :src="item.senderIcon" />
           </div>
         </div>
       </div>
       <!-- messge input -->
       <div class="footer">
-        <input id="mes-input" v-model="inputText"/>
+        <input id="mes-input" v-model="inputText" />
         <button @click="sendMsg">发送</button>
+        <button @click="clear">换名称</button>
       </div>
     </div>
   </div>
@@ -26,90 +36,92 @@ export default {
   name: "App",
   data() {
     return {
-      statusTips: '聊天室正在连接221....',
-      inputText: '',
-      messageList: []
-    }
+      statusTips: "聊天室正在连接221....",
+      userId: "",
+      inputText: "",
+      messageList: [],
+    };
   },
   created() {
-    const _this = this
-    _this.userInfo = window.localStorage.getItem("userInfo") || null
-    _this.createWS()
+    const _this = this;
+    _this.userId = window.localStorage.getItem("userId") || null;
+    console.log(_this.userId, "userid");
+    _this.createWS();
   },
   beforeDestroy() {
-    const _this = this
-    _this.ws.close()
+    const _this = this;
+    _this.ws.close();
   },
   methods: {
     createWS() {
-      const _this = this
+      const _this = this;
       if (window.WebSocket) {
-        _this.ws = new WebSocket(`ws://${window.location.hostname}:8001`)
+        _this.ws = new WebSocket(`ws://${window.location.hostname}:8001`);
+
         _this.ws.onopen = function () {
-          console.log('连接服务器成功');
-          _this.statusTips = '连接成功';
+          _this.statusTips = "连接成功";
           //登入消息
-          let param
-          try {
-            param = {
-              type: 'self',
-              userid: _this.userInfo.userid,
-              photo: _this.userInfo.photo,
-              username: _this.userInfo.username
-            }
-          } catch {
-            param = {
-              type: 'self'
-            }
-          }
-          _this.ws.send(JSON.stringify(param))
-        }
+          let param = {
+            type: "enter",
+            userId: _this.userId,
+          };
+          _this.sendRequest(param);
+        };
+
         _this.ws.onclose = function () {
-          console.log('服务器关闭');
-        }
+          console.log("服务器关闭");
+        };
+
         _this.ws.onerror = function () {
-          console.log('连接出错');
-        }
+          console.log("连接出错");
+        };
+
         _this.ws.onmessage = function (e) {
           // 接收信息
-          let resData = e.data
-          console.log("接受数据e", resData)
+          let resData = JSON.parse(e.data);
           //分配的个人信息写入本地
-          if(resData.type === 'self') {
-            _this.userInfo = resData
-            window.localStorage.setItem('userInfo', _this.userInfo)
-          } else if(resData.type === 'user') {//别人登入消息
-            
-          } else {//消息
-            _this.messageList.push(resData)
+          if (resData.type === "register") {
+            _this.userId = resData.data.userId;
+            window.localStorage.setItem("userId", _this.userId);
+          } else if (resData.type === "msgList") {
+            _this.messageList = resData.data;
+          } else {
+            //消息 or 通知
+            _this.messageList.push(resData.data);
           }
-          
-          
-        }
+        };
       }
+    },
+    sendRequest(param) {
+      const _this = this;
+      _this.ws && _this.ws.send(JSON.stringify(param));
     },
     sendMsg() {
       const _this = this;
-      let msg = _this.inputText
       let param = {
-        type: 'message',
-        msg,
-        userid: _this.userInfo.userid,
-        username: _this.userInfo.username
-      }
-      _this.ws && _this.ws.send(param)
-    }
-  }
-}
+        type: "msg",
+        content: _this.inputText,
+        senderId: _this.userId,
+        time: new Date().getTime(),
+      };
+      _this.sendRequest(param);
+      _this.inputText = "";
+    },
+    clear() {
+      window.localStorage.removeItem("userId");
+    },
+  },
+};
 </script>
 
 <style lang="less">
-*{
+* {
   box-sizing: border-box;
-  margin: 0; 
+  margin: 0;
   padding: 0;
 }
-html,body{
+html,
+body {
   width: 100%;
   height: 100%;
   overflow-x: hidden;
@@ -118,11 +130,14 @@ html,body{
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   height: 100vh;
   display: flex;
   flex-direction: column;
-  background:#4D4948;
+  background: #4d4948;
+  padding: constant(safe-area-inset-top) constant(safe-area-inset-right)
+    constant(safe-area-inset-bottom) constant(safe-area-inset-left);
+  padding: env(safe-area-inset-top) env(safe-area-inset-right)
+    env(safe-area-inset-bottom) env(safe-area-inset-left);
 }
 body {
   margin: 0;
@@ -132,38 +147,62 @@ body {
   border: 1px solid slategrey;
   display: flex;
   flex-direction: column;
+  overflow-y: auto;
 }
 .list-wrap {
   flex: 1;
+  .list {
+    margin-top: 20px;
+    .list-content {
+      display: flex;
+      .send {
+        position: relative;
+        min-width: 10px;
+        max-width: 250px;
+        min-height: 35px;
+        padding: 6px;
+        background: #f8c301;
+        border-radius: 5px;
+        //margin:20px auto 0;
+        .arrow {
+          position: absolute;
+          top: 5px;
+          right: -16px; /* 圆角的位置需要细心调试哦 */
+          width: 0;
+          height: 0;
+          font-size: 0;
+          border: solid 8px;
+          border-color: #4d4948 #4d4948 #4d4948 #f8c301;
+        }
+      }
+      .send.left {
+        background: #ffffff;
+        margin-left: 20px;
+        .arrow {
+          position: absolute;
+          top: 5px;
+          left: -16px; /* 圆角的位置需要细心调试哦 */
+          width: 0;
+          height: 0;
+          font-size: 0;
+          border: solid 8px;
+          border-color: #4d4948 #ffffff #4d4948 #4d4948;
+        }
+      }
+      .icon {
+        width: 30px;
+        height: 30px;
+      }
+    }
+  }
 }
 .footer {
   width: 100vw;
   display: flex;
-}
-.footer input {
-  width: 100%;
-  height: 40px;
-  border: 1px solid slategrey;
-}
-
-.send {
-  position:relative;
-  width:150px;
-  height:35px;
-  padding: 6px;
-  background:#F8C301;
-  border-radius:5px; /* 圆角 */
-  margin:20px auto 0;
-}
-
-.send .arrow {
-  position:absolute;
-  top:5px;
-  right:-16px; /* 圆角的位置需要细心调试哦 */
-  width:0;
-  height:0;
-  font-size:0;
-  border:solid 8px;
-  border-color:#4D4948 #4D4948 #4D4948 #F8C301;
+  input {
+    width: 100%;
+    height: 40px;
+    border: 1px solid slategrey;
+  }
 }
 </style>
